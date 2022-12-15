2d827010e4026581ecc6d4c0e6bf24028e5c83a3
alohaquando <aloha.quando@gmail.com>
2022-12-14 23:50:32 -0500

    Font working

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,16 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
-
 const config = {
   content: ["./src/**/*.{html,js,svelte,ts}"],
 
   theme: {
+
+    fontFamily: {
+      sans: [
+        "Strawford", { fontFeatureSettings: '"tnum", "lnum", "salt", "ss01"' }
+      ]
+    },
+
     extend: {
-      fontFamily: {
-        "sans": ["Strawford", { fontFeatureSettings: '"tnum", "lnum", "salt", "ss01"' }, ...defaultTheme.fontFamily.sans]
-      }
+
     }
   },
 
