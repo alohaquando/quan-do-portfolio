b65a6eafc9449a024199bbe37d7ca52502b276ea
alohaquando <aloha.quando@gmail.com>
2022-12-14 21:06:32 -0500

    Added images and font

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+import { imagetools } from 'vite-imagetools';
 
-/** @type {import('vite').UserConfig} */
+/** @type {import("vite").UserConfig} */
 const config = {
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), imagetools()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
