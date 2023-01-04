5506246d03c1a2eb371bfe66a5dd9b169d68c2a4
alohaquando <aloha.quando@gmail.com>
2023-01-03 20:39:29 -0500

    Updated ContactCard

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -4,8 +4,8 @@
 
 	let contactLinks = [
 		{ href: 'mailto:work@quanhdo.com', icon: 'envelope', title: 'Email', subtitle: 'work@quanhdo.com', colorStyle: 'bg-amber-400 dark:bg-amber-600', ringStyle: 'group-hover:ring-amber-400 dark:group-hover:ring-amber-600' },
-		{ href: 'https://github.com/alohaquando', icon: 'github', title: 'GitHub', subtitle: '@alohaquando', colorStyle: 'bg-violet-400 dark:bg-violet-600', ringStyle: 'group-hover:ring-violet-400 dark:group-hover:ring-violet-600' },
-		{ href: '/contact', icon: 'linkedin', title: 'LinkedIn', subtitle: '@', colorStyle: 'bg-blue-400 dark:bg-blue-600', ringStyle: 'group-hover:ring-blue-400 dark:group-hover:ring-blue-600' }
+		{ href: 'github.com/alohaquando', icon: 'github', title: 'GitHub', subtitle: '@alohaquando', colorStyle: 'bg-violet-400 dark:bg-violet-600', ringStyle: 'group-hover:ring-violet-400 dark:group-hover:ring-violet-600' },
+		{ href: 'linkedin.com/in/quanhoangdo', icon: 'linkedin', title: 'LinkedIn', subtitle: '/quanhoangdo', colorStyle: 'bg-blue-400 dark:bg-blue-600', ringStyle: 'group-hover:ring-blue-400 dark:group-hover:ring-blue-600' }
 	];
 </script>
 
